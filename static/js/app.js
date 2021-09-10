8d914746b71e2d4c65098052317c05e0ad05d6f5
Kathy Bauman <kathybmans@gmail.com>
2021-09-10 17:18:49 -0500

    modify app.js

--- static/js/app.js
+++ static/js/app.js
@@ -26,3 +26,36 @@ function buildTable(data) {
       );
     });
   }
+
+// 11.5.3
+// add date function to click
+// d3.select is lookin for the "datetime" id in HTML tags
+// property("value") gets value and holds in date variable
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+ };
+
+// Attach an event to listen for the form button - on click, run the handleClick function
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads - with original data
+buildTable(tableData);
+
+
+
+
